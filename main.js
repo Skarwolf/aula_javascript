1c8670c75359c3e2feb7fc92357a10bfa2a9b738
Skarwolf <Lucas.Nunes2500@hotmail.com>
2021-02-01 15:04:24 -0300

    Aulas Introdutorias - DIO

---
+++ main.js
@@ -0,0 +1,89 @@
+
+var d = new Date();
+alert(d.getMonth()); /* Mês */ 
+alert(d.getMinutes()); /* Minutos */ 
+alert(d.getHours()); /* Horas */
+alert(d.getSeconds()); /* Segundo */
+
+/*
+var d = new Date(); 
+alert(d);
+*/
+
+
+/*
+var count = 0;
+for(count = 0; count <= 5; count ++){
+    console.log(count);
+    alert(count);
+}
+*/
+
+
+
+
+
+/*
+Laços de repetição 
+var count = 0;
+while (count < 5){
+    console.log(count);
+    alert(count);
+    count = count + 1; /pode ser trocado por "count ++"
+}
+
+
+
+
+
+Estrutura Condicional (Boleana);
+var idade = prompt("Qual é sua Idade? ");
+var idade = 17; 
+if (idade >= 18){
+    alert("Esse garoto é maior de idade");
+}else{
+    alert("ele é menor de idade");
+}
+
+
+
+Dicionario
+var minerio2 = [{nome:"ferro", cor:"Cinza Prateado"},{nome:"ferro", cor:"Cinza Prateado"}];
+alert(minerio2[1].cor);
+
+
+Array - Lista 
+var minerio = {nome:"ferro", cor:"Cinza Prateado"};
+console.log(minerio.nome);
+alert(minerio.cor);
+
+
+Lista de Frutas 
+var lista = ["Pedra","Madeira","Ferro"];
+lista.push("Ouro"); / Adiciona algo na lista "forçadamente"
+lista.pop(""); / retira o que o push adiciono
+console.log(lista[0]);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" | "));
+alert(lista);
+
+
+var nome = "Lucas Aguiar"
+var idade =20;
+var frase = "Japão é o melhor time do Mundo"
+
+alert(nome +  " tem " + idade + " anos de idade ")
+
+console.log(frase.replace("Japão", "Brasil"));
+console.log(nome);
+console.log(idade);
+alert(frase.replace("Japão", "Brasil"));
+
+var n1 = 5; 
+var n2 = 10;
+var n3 = n1 + n2; 
+alert(n3)
+
+*/
